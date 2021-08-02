5e64627c6814ba894ebba3aada04d83f5c997ec1
Dung Nguyen <dung.nguyen@onpoint.vn>
2021-08-02 22:11:58 +0700

    Update mix

--- mix.exs
+++ mix.exs
@@ -10,7 +10,10 @@ defmodule Valdi.MixProject do
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
-        coveralls: :test
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
       ],
       docs: docs(),
       name: "Valdi",
