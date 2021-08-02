899bc697d69512280dfce00d0b0bdc9dd13f8894
Dung Nguyen <dung.nguyen@onpoint.vn>
2021-08-02 11:27:52 +0700

    update package name

--- mix.exs
+++ mix.exs
@@ -4,12 +4,12 @@ defmodule Valdi.MixProject do
   def project do
     [
       app: :valdi,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: docs(),
-      name: "Magik",
+      name: "Valdi",
       description: description(),
       source_url: "https://github.com/onpointvn/valdi",
       package: package()
