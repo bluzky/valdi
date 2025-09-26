32862e4b6e8f6f35920b01dff6d73ed1986b6218
Always blue <bluesky.1289@gmail.com>
2025-09-26 09:18:54 +0700

    Release v0.5.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Valdi.MixProject do
   def project do
     [
       app: :valdi,
-      version: "0.5.0",
+      version: "0.5.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
